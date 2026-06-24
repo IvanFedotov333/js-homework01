676564991aae610ce202ac48604d507a0b41893f
IvanFedotov333 <ivan_fedotov_2013@inbox.ru>
2026-06-24 11:50:37 +0300

    Выполнено ДЗ 1: переменные, alert, console.log

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "Заголовок";
+const screens = "Экраны";
+const screenPrice = 5000;
+const rollBack = 200;
+const fullPrice = screenPrice - rollBack;
+const adaptive = true;
+
+alert("Всем привет!");
+
+console.log("Итоговая цена:", fullPrice);
