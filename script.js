99909d33fdc09a7fc2faab35f7a5ba4fe795d991
IvanFedotov333 <ivan_fedotov_2013@inbox.ru>
2026-06-26 19:49:31 +0300

    Домашнее задание №3

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let title = prompt("Как называется ваш проект?");
+
+let screens = prompt("Какие типы экранов нужно разработать?");
+
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+
+let service2 = prompt("Какой ещё дополнительный тип услуги нужен?");
+
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+
+const totalPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let agentPercent = 10; // процент комиссии
+
+let agentFee = (totalPrice * agentPercent) / 100; // откат посреднику
+
+const servicePercentPrice = Math.ceil(totalPrice - agentFee);
+
+console.log(`Общая цена услуги - ${servicePercentPrice} рублей`);
+
+if (totalPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (15000 < totalPrice && totalPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else {
+  console.log("Скидка не предусмотрена");
+}
